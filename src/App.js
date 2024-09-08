f7061a7b553fb36d3f75560e7c9474902d5d3ab4
Furkan Mert Mısırlı <misirlifu@mef.edu.tr>
2024-09-08 16:12:25 +0300

    add router

--- src/App.js
+++ src/App.js
@@ -1,34 +1,66 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
-import Todo from './components/Todo'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
+
+// Import your components here
+import Todo from './components/Todo';
 import FetchData from './components/Fetch';
 
+// Main App Component
 function App() {
   return (
-    
-    <div className="App">
+    <Router>
+      <div>
+        {/* Navigation Bar */}
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container">
+            <Link className="navbar-brand" to="/">Home</Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/about">About</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/users">Users</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
 
-      <FetchData />
-      <Todo />
-
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        {/* Routing */}
+        <div className="container mt-4">
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
+}
+
+// Page Components
+function Home() {
+  return <Todo/>;
+}
+
+function About() {
+  return <FetchData/>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
 }
 
 export default App;
